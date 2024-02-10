22f18121ffdc5a7ddadec9b0c0d281cbef198671
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2024-02-10 12:26:32 +0100

    added Link to the create page

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import Link from "next/link";
 import TicketList from "./TicketList";
 import Loading from "../loading";
 
@@ -9,13 +10,21 @@ export default function Tickets() {
 				<div>
 					<h2>Tickets</h2>
 					<p>
-						<small>Currently open tickets</small>
+						<small>Currently new tickets</small>
 					</p>
 				</div>
 			</nav>
 			<Suspense fallback={<Loading />}>
 				<TicketList />
 			</Suspense>
+			<div className="text-center mt-10">
+				<Link
+					href="/tickets/create"
+					className="bg-primary text-white font-medium text-sm px-3 py-2 rounded-lg"
+				>
+					Create New Ticket
+				</Link>
+			</div>
 		</main>
 	);
 }
